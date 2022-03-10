ac1d9f7bf8ef6d42bfb29a057c6c2ff2a072e28d
LW <2771004336@qq.com>
2022-03-11 03:13:22 +0800

    重命名和新增爬虫的模板

--- tools/make_template.py
+++ tools/make_template.py
@@ -1,29 +1,34 @@
 from string import Template
 
 
-def tmpl_init():
-    filePath = '../parser_conf_test.py'
-    class_file = open(filePath, 'w')
+def tmpl_init(d: dict):
+    # filePath = '../parser_conf_test.py'
 
-    mycode = []
+    for k, v in d.items():
+        filePath = '../' + v
+        class_file = open(filePath, 'w')
 
-    # 加载模板文件
-    template_file = open('../conf/is_tmpl.tmpl', 'r')
-    tmpl = Template(template_file.read())
+        mycode = []
 
-    # 模板替换
-    mycode.append(tmpl.substitute(
-        CLASSNAME='DEFAULT',
-        Class_Name='Default',
-        En_name='mystruct',
-        Type='int',
-        Name='value'))
+        # 加载模板文件
+        # template_file = open('../conf/isparser_tmpl.tmpl', 'r')
+        template_file = open('../conf/' + k, 'r')
+        tmpl = Template(template_file.read())
 
-    # 将代码写入文件
-    class_file.writelines(mycode)
-    class_file.close()
+        # 模板替换
+        mycode.append(tmpl.substitute(
+            CLASSNAME='DEFAULT',
+            Class_Name='Default',
+            En_name='mystruct',
+            Type='int',
+            Name='value'))
+
+        # 将代码写入文件
+        class_file.writelines(mycode)
+        class_file.close()
 
 
 if __name__ == '__main__':
-    tmpl_init()
+    tmpl = {"isparser_tmpl.tmpl": "parser_conf_test.py", "isspider_tmpl.tmpl": "spider_test.py"}
+    tmpl_init(tmpl)
     print('Create template successfully')
