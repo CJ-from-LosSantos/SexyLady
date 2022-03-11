867fdc5e698e3dc6414cc4a1bb698b1e90570935
神经蛙 <2771004336@qq.com>
2022-03-11 17:01:26 +0000

    删除文件 parser_conf.py

--- parser_conf.py
+++
@@ -1,9 +0,0 @@
-class Parser:
-    name = 'htmlxpath'
-
-    def __init__(self, method):
-        self.html = method
-
-    def parser_source(self):
-        hot = self.html.xpath('//*[@id="__next"]/div[1]/div/div/section[1]/article/h2[1]/text()')
-        print(hot)
