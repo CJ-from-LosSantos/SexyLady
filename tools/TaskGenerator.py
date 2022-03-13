d5905708dd5eff25a856cf29998d4cc87476cee5
LW <2771004336@qq.com>
2022-03-13 19:15:03 +0800

    增加upload_task方法 上传任务链接和模板名到redis中

--- tools/TaskGenerator.py
+++ tools/TaskGenerator.py
@@ -1,5 +1,54 @@
 import asyncio
+import inspect
+from conf.config import REDIS
 from tools.LogGenerator import ML
+from redis import StrictRedis
+
+
+def upload_task(tmplName=None, taskUrl=None):
+    """
+    负责上传 url 和对应爬虫模板的 ”set_spider.py中的类名“ 到 redis 中
+    :param tmplName: 对应爬虫模板的类名，用户如果没有传递则获取所有爬虫模板下的所有类名
+    :param taskUrl: 任务url
+    :return:
+    """
+
+    _redis = StrictRedis(*REDIS)
+    pl = _redis.pipeline()
+
+    if tmplName:
+        ok = _redis.hsetnx('Task_table', tmplName, taskUrl)
+        if not ok:
+            ML.debug('Task exists')
+        else:
+            ML.debug('Task: %s insertion complete' % {'Field': tmplName, 'Value': taskUrl})
+        return 1
+
+    classes = []  # 爬虫模板列表(类名)
+    success = []  # 添加任务成功列表,用于日志显示
+    fail = []  # 添加任务失败列表,用于日志显示
+
+    from tmpl import set_spiders
+    clsmembers = inspect.getmembers(set_spiders, inspect.isclass)
+    for (name, _) in clsmembers:
+        """
+        Only：
+            one spider tmpl --> one set_parser file
+            one spider tmpl --> many set_parser file
+        """
+        pl.hsetnx('Task_table', name, taskUrl)
+        classes.append(name)
+    end_execute = pl.execute()
+
+    for inx, YN in enumerate(end_execute):
+        if YN:
+            success.append(classes[inx])
+        else:
+            fail.append(classes[inx])
+    ML.debug('Task list: %s insertion complete' % {'Field': success})
+    ML.debug('Task list: %s exists' % {'Field': fail})
+
+    return 1
 
 
 def generate_async_task(async_function, urls: list) -> list:
@@ -8,7 +57,7 @@ def generate_async_task(async_function, urls: list) -> list:
     except:
         ML.exception('ERROR Snapshot')
     finally:
-        ML.info('Creation Task is Completed, have %d task' % len(urls))
+        ML.debug('Creation Task is Completed, have %d task' % len(urls))
 
 
 def generate_task(_function, urls: list) -> list:
@@ -17,7 +66,7 @@ def generate_task(_function, urls: list) -> list:
     except:
         ML.exception('ERROR Snapshot')
     finally:
-        ML.info('Creation Task is Completed, have %d task' % len(urls))
+        ML.debug('Creation Task is Completed, have %d task' % len(urls))
 
 
 def async_run_loop(tasks):
