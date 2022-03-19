2e006755ce2f35423021ac2628d9e105b24956fe
CJ <2771004336@qq.com>
2022-03-20 00:35:23 +0800

    更正

--- conf/config.py
+++ conf/config.py
@@ -1,3 +1,4 @@
 # 使用时，请先使用微信扫描 ”微信机器人测试二维码“ 加入企业才可以使用
-TOKEN = ('ww2c730f9fc390fa1f', '6rPE5i9bUFoc3sFCWWaV6Ux_fzUm3AX9TpgZaj6XwFo', 1000002)
-REDIS = ('localhost', 6379, 0)
+# TOKEN = ('ww2c730f9fc390fa1f', '6rPE5i9bUFoc3sFCWWaV6Ux_fzUm3AX9TpgZaj6XwFo', 1000002)
+# REDIS = ('localhost', 6379, 0)
+# MONGO = 'mongodb://localhost:27017/'
