c82695c223390ff143661ae80c728180b5b2cd62
CJ <2771004336@qq.com>
2022-03-20 22:44:12 +0800

    暂时移除

--- tools/LogGenerator.py
+++ tools/LogGenerator.py
@@ -1,11 +1,11 @@
-from loguru import logger
-
-ML = logger
-
-
-def make_logfile(file_name, rotation="1 week", retention="10 days"):
-    """
-    简单的初始化log的文件
-    使用帮助: https://pythondict.com/life-intelligent/tools/loguru/
-    """
-    return ML.add("%s_.log" % file_name, rotation=rotation, retention=retention)
+# from loguru import logger
+#
+# ML = logger
+#
+#
+# def make_logfile(file_name, rotation="1 week", retention="10 days"):
+#     """
+#     简单的初始化log的文件
+#     使用帮助: https://pythondict.com/life-intelligent/tools/loguru/
+#     """
+#     return ML.add("%s_.log" % file_name, rotation=rotation, retention=retention)
